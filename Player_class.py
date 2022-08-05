eef1eb5ad8d931836b33e49ee12b5fa1c3a04a62
YuaHyodo <66828980+YuaHyodo@users.noreply.github.com>
2022-08-06 00:00:49 +0900

    Add files via upload

---
+++ Player_class.py
@@ -0,0 +1,81 @@
+import json
+import time
+
+class Player:
+    def __init__(self, connect, name, password):
+        self.connect = connect#接続
+        self.name = name#名前
+        self.password = password#パスワード
+        self.buf_size = 1024#バフサイズ
+        self.load_player_data()#プレーヤーデータの読み込み
+
+    def update_player_data(self, result):
+        #リザルトを反映させる
+        self.player_data['games'] += 1 
+        if result == 'win':
+            self.player_data['wins'] += 1
+        elif result == 'draw':
+            self.player_data['draws'] += 1
+        elif result == 'lose':
+            self.player_data['losses'] += 1
+        return
+
+    def load_player_data(self):
+        #プレーヤー一覧に名前があるか？
+        with open('./Players/Players.json', 'r') as f:
+            players = json.load(f)
+        self.new_player = False
+        if self.name in players.keys():
+            #あるならデータを読み込んで終わる
+            self.path = players[self.name]
+            with open(self.path, 'r') as f:
+                self.player_data = json.load(f)
+            return
+        #無いなら初期値を設定する
+        self.new_player = True 
+        self.path = './Players/Player/' + self.name + '.json'
+        self.player_data = {'rate': 1500, 'games': 0, 'wins': 0, 'draws': 0, 'losses': 0}
+        return
+
+    def write_player_data(self):
+        #もし、新規プレーヤーならプレーヤー一覧に入れる必要がある
+        if self.new_player:
+            with open('./Players/Players.json', 'r') as f:
+                players = json.load(f)
+            players[self.name] = self.path
+            with open('./Players/Players.json', 'w') as f:
+                players = json.dump(players, f)
+        #反映したものを記録
+        with open(self.path, 'w') as f:
+            json.dump(self.player_data, f)
+        return
+
+    def send_message(self, message):
+        #メッセージを送る関数
+        if '\n' not in message:
+            #改行をつける
+            message += '\n'
+        self.connect.send(message.encode('utf-8'))
+        return
+
+    def recv_message(self):
+        #メッセージを受け取る関数
+        message = self.connect.recv(self.buf_size).decode('utf-8')
+        return message
+
+    def get_move(self):
+        #指し手を受け取る関数
+        #また、消費時間を計測している
+        start_time = time.time()
+        while True:
+            move = self.recv_message()
+            if 'TORYO' in move:#投了
+                return '%TORYO', int(time.time() - start_time)
+            if 'PASS' in move:#パス
+                return 'pass', int(time.time() - start_time)
+            if len(move) >= 2:#指し手
+                break
+        #とりあえず動けば良い
+        return move[1:3], int(time.time() - start_time)
+
+    
---
+++ play_game_v1.py
@@ -0,0 +1,248 @@
+"""
+メッセージをlistに格納して、それを順番に送るようにすると
+まだマシになりそうなので、あとでやる。
+"""
+import creversi as reversi
+import time
+
+from rate_v1 import update_rate
+#改行を表す
+k = '\n'
+
+class game:
+    def __init__(self, player1, player2, time_setting={'total': 300, 'inc': 10}):
+        self.player1 = player1#先手
+        self.player2 = player2#後手
+        self.time_setting = time_setting#時間設定
+        self.ID = 'game-' + player1.name + '-VS-' +player2.name + '-' + str(time.time()).replace('.', '')#ゲームID
+        self.file_name = './games/' + self.ID + '.txt'#保存先ファイル名
+        self.file_text = ''#ファイルに書き込む文章
+
+    def write(self):#ファイルに書き込む
+        with open(self.file_name, 'w') as f:
+            f.write(self.file_text)
+        return
+
+    def start(self):#startという名前だが、これで完結している
+        #ゲームサマリー(ゲームの情報に関するメッセージ)を各プレーヤに送信
+        self.send_gamesummary(player_color='+')
+        self.send_gamesummary(player_color='-')
+        agree = [False, False]
+        #返事待ち
+        while True:
+            m = self.player1.recv_message()
+            if 'AGREE' in m:
+                agree[0] = True
+                break
+            if 'REJECT' in m:
+                break
+        while True:
+            m = self.player2.recv_message()
+            if 'AGREE' in m:
+                agree[1] = True
+                break
+            if 'REJECT' in m:
+                break
+        #両方agreeしたか？
+        if agree[0] and agree[1]:
+            #したのでスタート
+            self.player1.send_message('START' + k)
+            self.player2.send_message('START' + k)
+        else:
+            #どちらかが拒否したので停止
+            if not agree[0]:
+                r_name = self.player1.name
+            else:
+                r_name = self.player2.name
+            self.player1.send_message('REJECT' + k)
+            self.player2.send_message('REJECT' + k)
+            return 'REJECT'
+        #開始
+        self.main_loop()
+        return
+
+    def send_gamesummary(self, player_color='+'):
+        summary = 'BEGIN Game_Summary'
+        summary += k
+        summary += 'Protocol_Version: 0.0.1'
+        summary += k
+        summary += ('Game_ID:' + self.ID)
+        summary += k
+        summary += ('Name+:' + self.player1.name)
+        summary += k
+        summary += ('Name-:' + self.player2.name)
+        summary += k
+        summary += ('Your_Turn:' + player_color)
+        summary += k
+        summary += 'To_Move:+'
+        summary += k
+        summary += 'BEGIN Time'
+        summary += k
+        summary += ('Total_Time:' + str(self.time_setting['total']))
+        summary += k
+        summary += ('Increment:' + str(self.time_setting['inc']))
+        summary += k
+        summary += 'END Time'
+        summary += k
+        summary += 'BEGIN Position'
+        summary += k
+        summary += 'position startpos'
+        summary += k
+        summary += 'END Position'
+        summary += k
+        summary += 'END Game_Summary'
+        summary += k
+        if player_color == '+':
+            self.player1.send_message(summary)
+        else:
+            self.player2.send_message(summary)
+        self.file_text += summary
+        return
+
+    def return_winner(self, board):
+        my = board.piece_num()
+        opponent = board.opponent_piece_num()
+        if my == opponent:
+            return 'draw'
+        if my > opponent:
+            if board.turn:
+                return 'black'
+            return 'white'
+        if board.turn:
+            return 'white'
+        return 'black'
+
+    def main_loop(self):
+        #メインループ(不完全)
+        #合法手チェック用
+        board = reversi.Board()
+        #残りの持ち時間
+        player1_time = self.time_setting['total']
+        player2_time = self.time_setting['total']
+        #色々
+        result = [None, None]
+        moves_list = []
+        while True:
+            #先手の行動
+            move, t = self.player1.get_move()
+            #時間などの管理
+            player1_time -= t
+            player1_time += self.time_setting['inc']
+            #時間が切れたか？
+            if player1_time <= 0:
+                self.player1.send_message('#TIME_UP')
+                self.player2.send_message('#TIME_UP')
+                self.player1.send_message('#LOSE')
+                self.player2.send_message('#WIN')
+                self.file_text += ('%TIME_UP' + k)
+                result = ['lose', 'win']
+                break
+            #投了したか？
+            if move == '%TORYO':
+                self.player1.send_message('#RESIGN')
+                self.player2.send_message('#RESIGN')
+                self.player1.send_message('#LOSE')
+                self.player2.send_message('#WIN')
+                self.file_text += ('%TORYO' + k)
+                result = ['lose', 'win']
+                break
+            #手番を変更
+            message = move + ',T' + str(t)
+            self.player1.send_message(message)
+            self.player2.send_message(message)
+            c_move = reversi.move_from_str(move)
+            if c_move not in list(board.legal_moves):
+                self.player1.send_message('#ILLEGAL_MOVE')
+                self.player2.send_message('#ILLEGAL_MOVE')
+                self.player1.send_message('#LOSE')
+                self.player2.send_message('#WIN')
+                self.file_text += ('%ILLEGAL_MOVE' + k)
+                result = ['lose', 'win']
+                break
+            board.move(c_move)
+            self.file_text += (move + k)
+            if board.is_game_over():
+                winner = self.return_winner(board)
+                self.player1.send_message('#DOUBLE_PASS')
+                self.player2.send_message('#DOUBLE_PASS')
+                if winner == 'draw':
+                    self.player1.send_message('#DRAW')
+                    self.player2.send_message('#DRAW')
+                    result = ['draw', 'draw']
+                if winner == 'black':
+                    self.player1.send_message('#WIN')
+                    self.player2.send_message('#LOSE')
+                    result = ['win', 'lose']
+                if winner == 'white':
+                    self.player1.send_message('#LOSE')
+                    self.player2.send_message('#WIN')
+                    result = ['lose', 'win']
+                self.file_text += ('%DOUBLE_PASS' + k)
+                break
+            #後手番
+            move, t = self.player2.get_move()
+            player2_time -= t
+            player2_time += self.time_setting['inc']
+            if player2_time <= 0:
+                self.player2.send_message('#TIME_UP')
+                self.player1.send_message('#TIME_UP')
+                self.player2.send_message('#LOSE')
+                self.player1.send_message('#WIN')
+                self.file_text += ('%TIME_UP' + k)
+                result = ['win', 'lose']
+                break
+            if move == '%TORYO':
+                self.player2.send_message('#RESIGN')
+                self.player1.send_message('#RESIGN')
+                self.player2.send_message('#LOSE')
+                self.player1.send_message('#WIN')
+                self.file_text += ('%TORYO' + k)
+                result = ['win', 'lose']
+                break
+            message = move + ',T' + str(t)
+            self.player2.send_message(message)
+            self.player1.send_message(message)
+            c_move = reversi.move_from_str(move)
+            if c_move not in list(board.legal_moves):
+                self.player2.send_message('#ILLEGAL_MOVE')
+                self.player1.send_message('#ILLEGAL_MOVE')
+                self.player2.send_message('#LOSE')
+                self.player1.send_message('#WIN')
+                self.file_text += ('%ILLEGAL_MOVE' + k)
+                result = ['win', 'lose']
+                break
+            board.move(c_move)
+            self.file_text += (move + k)
+            if board.is_game_over():
+                winner = self.return_winner(board)
+                self.player1.send_message('#DOUBLE_PASS')
+                self.player2.send_message('#DOUBLE_PASS')
+                if winner == 'draw':
+                    self.player1.send_message('#DRAW')
+                    self.player2.send_message('#DRAW')
+                    result = ['draw', 'draw']
+                if winner == 'black':
+                    self.player1.send_message('#WIN')
+                    self.player2.send_message('#LOSE')
+                    result = ['win', 'lose']
+                if winner == 'white':
+                    self.player1.send_message('#LOSE')
+                    self.player2.send_message('#WIN')
+                    result = ['lose', 'win']
+                self.file_text += ('%DOUBLE_PASS' + k)
+                break
+        #棋譜を保存
+        self.write()
+        #リザルトを反映
+        self.player1.update_player_data(result[0])
+        self.player2.update_player_data(result[1])
+        d = {'win': 1, 'draw': 0.5, 'lose': 0}
+        r = [d[result[0]], d[result[1]]]
+        rate1, rate2 = update_rate(r, self.player1.player_data['rate'], self.player2.player_data['rate'])
+        self.player1.player_data['rate'] = rate1
+        self.player2.player_data['rate'] = rate2
+        self.player1.write_player_data()
+        self.player2.write_player_data()
+        return
+
+    
